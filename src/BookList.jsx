e7bcef324775b37887cd4622bb7dee2e1b3dd701
jihad-islam <jihadislam.diu@gmail.com>
2025-03-19 00:34:05 +0600

    a

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -4,18 +4,27 @@ import BookRow from "./BookRow";
 import PropTypes from "prop-types";
 
 function BookList({ searchTerm, books, onFeatureBook }) {
-  const rows = [];
+  /*
+    const rows = [];
+    books.forEach((book) => {
+      if (book.title.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1) {
+        return;
+      }
 
-  books.forEach((book) => {
-    if (book.title.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1) {
-      return;
-    }
+      // else condition
+      rows.push(
+        <BookRow key={book.id} book={book} onFeatureBook={onFeatureBook} />
+      );
+    });
+  */
 
-    // else condition
-    rows.push(
-      <BookRow key={book.id} book={book} onFeatureBook={onFeatureBook} />
-    );
-  });
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const rows = filteredBooks.map((book) => (
+    <BookRow key={book.id} book={book} onFeatureBook={onFeatureBook} />
+  ));
 
   return <div className="space-y-4">{rows}</div>;
 }
